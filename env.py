ac10ee972a4066e65a00c16a58ab16bbd617ae62
Rejoan Islam Afridi <47922447+rejoan-afridi@users.noreply.github.com>
2020-10-21 22:10:25 +0600

    Create env.py

---
+++ env.py
@@ -0,0 +1,8 @@
+DB_CONFIG = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'data_collection',
+    'USER': 'root',
+    'PASSWORD': 'at0991122',
+    'HOST': 'localhost',
+    'PORT': '3306'
+}
